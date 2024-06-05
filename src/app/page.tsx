c2d7bfc22f0f257027f1b3a42fde9cca0f747c19
ralf tamer <ralf.tamer@gmail.com>
2024-06-06 01:43:00 +0300

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { CSSProperties } from "react";
 
 import "swiper/css";
 import SwiperWrapper from "../components/SwiperWrapper";
+import Image from "next/image";
 
 export default function Home() {
   const darkBlueColor = "#104d6c";
@@ -31,69 +32,140 @@ export default function Home() {
         <div className="absolute" style={{ top: 0 }}>
           <div className="" style={{ width: "100vw" }}>
             <div className="flex justify-center pt-4">
-              <img
-                alt="image"
+              <Image
                 className="max-md:w-[60px] md:w-[200px]"
-                src="/images/TopLogo.png"
-                style={{ objectFit: "contain" }}
+                src={"/images/TopLogo.png"}
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                  maxWidth: "60px",
+                }}
               />
             </div>
-            <img
-              alt="image"
+
+            <Image
               className="pt-8"
               src="/images/Header.png"
-              style={{ objectFit: "contain", width: "75vw" }}
+              alt="image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority
+              style={{
+                width: "75vw",
+                height: "100%",
+                //
+                objectFit: "contain",
+              }}
             />
             <div className="flex justify-center">
-              <img
-                alt="image"
+              <Image
                 className="pt-8"
                 src="/images/AboutMe.png"
-                style={{ objectFit: "contain", width: "80vw" }}
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "80vw",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                }}
               />
             </div>
             <div className="">
-              <img
-                alt="image"
+              <Image
                 className="pt-2"
                 src="/images/WhatYK.png"
-                style={{ objectFit: "contain", width: "30vw" }}
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "30vw",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                }}
               />
             </div>
             <div className="flex justify-center mt-[-2%]">
-              <img
-                alt="image"
+              <Image
                 className=""
                 src="/images/Iceberg.png"
-                style={{ objectFit: "contain", width: "70vw" }}
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "70vw",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                }}
               />
             </div>
             <div className="flex justify-end mt-[-20%]">
-              <img
-                alt="image"
+              <Image
                 className=""
                 src="/images/WhatYDK.png"
-                style={{ objectFit: "contain", width: "30vw" }}
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "30vw",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                }}
               />
             </div>
           </div>
         </div>
         <div className="flex justify-center">
-          <img
-            alt="image"
-            className=""
+          <Image
+            className="pt-2"
             src="/images/MyWork.png"
-            style={{ objectFit: "contain", width: "80vw" }}
+            alt="image"
+            width={0}
+            height={0}
+            sizes="80vw"
+            priority
+            style={{
+              width: "80vw",
+              height: "100%",
+              //
+              objectFit: "contain",
+            }}
           />
         </div>
         <div className="flex mt-[-2%] justify-center">
-          <img
-            alt="image"
+          <Image
             className=""
             src="/images/Diamonds.png"
+            alt="image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority
             style={{
-              objectFit: "contain",
               width: "70vw",
+              height: "100%",
+              //
+              objectFit: "contain",
             }}
           />
         </div>
@@ -101,31 +173,58 @@ export default function Home() {
         <SwiperWrapper>
           {myWorkImages.map((image) => {
             return (
-              <img
-                alt="image"
-                width={"95%"}
+              <Image
                 key={image}
                 src={`/images/my-work/${image}`}
                 className="swiper-lazy"
+                alt="image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                style={{
+                  width: "95%",
+                  height: "100%",
+                  //
+                  objectFit: "contain",
+                }}
               />
             );
           })}
         </SwiperWrapper>
         <div className="flex justify-center">
-          <img
-            alt="image"
+          <Image
             className=""
             src="/images/ContactMe.png"
-            style={{ objectFit: "contain", width: "80vw" }}
+            alt="image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority
+            style={{
+              width: "80vw",
+              height: "100%",
+              //
+              objectFit: "contain",
+            }}
           />
         </div>
         <div className="space-y-2">
           <div className="flex justify-center">
-            <img
-              alt="image"
+            <Image
               className="py-2"
               src="/images/Ready.png"
-              style={{ objectFit: "contain", width: "30vw" }}
+              alt="image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority
+              style={{
+                width: "30vw",
+                height: "100%",
+                //
+                objectFit: "contain",
+              }}
             />
           </div>
           <div className="mx-[10vw]">
@@ -134,7 +233,20 @@ export default function Home() {
                 className="col-span-4 pr-3 "
                 style={{ borderRight: `1px solid ${darkBlueColor}` }}
               >
-                <img alt="image" src="/images/Map.png" />
+                <Image
+                  className=""
+                  src="/images/Map.png"
+                  alt="image"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  priority
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    //
+                  }}
+                />
                 <div
                   className="max-md:text-[10px] md:text-[36px] max-md:leading-[11px] md:leading-[36px] pt-1"
                   style={{
@@ -151,31 +263,60 @@ export default function Home() {
                 style={{ borderRight: `1px solid ${darkBlueColor}` }}
               >
                 <div className={socialsClassName}>
-                  <img
-                    alt="image"
+                  <Image
+                    className=""
                     src="/images/Insta.png"
-                    className="max-md:w-[18px]"
+                    alt="image"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    priority
+                    style={{
+                      width: "18px",
+                      height: "100%",
+                      //
+                    }}
                   />
                   <div style={socialsStyle}>@ramyfch</div>
                 </div>
 
                 <div className={socialsClassName}>
-                  <img
-                    alt="image"
+                  <Image
+                    className=""
                     src="/images/Mail.png"
-                    className="max-md:w-[18px]"
+                    alt="image"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    priority
+                    style={{
+                      width: "18px",
+                      height: "100%",
+                      //
+                    }}
                   />
+
                   <div className="max-md:leading-[8px] md:leading-[22px]">
                     <div style={socialsStyle}>ramy_f_chrabieh</div>
                     <div style={socialsStyle}>@hotmail.com</div>
                   </div>
                 </div>
                 <div className={socialsClassName}>
-                  <img
-                    alt="image"
+                  <Image
+                    className=""
                     src="/images/Phone.png"
-                    className="max-md:w-[18px]"
+                    alt="image"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    priority
+                    style={{
+                      width: "18px",
+                      height: "100%",
+                      //
+                    }}
                   />
+
                   <div style={socialsStyle}>+961 79 145 441</div>
                 </div>
               </div>
@@ -203,10 +344,19 @@ export default function Home() {
                     style={{ aspectRatio: 1 }}
                     href="mailto:ramy_f_chrabieh@hotmail.com"
                   >
-                    <img
-                      alt="image"
-                      className=" md:w-[200px]"
+                    <Image
+                      className=""
                       src="/images/RedirectSquare.png"
+                      alt="image"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      priority
+                      style={{
+                        width: "200px",
+                        height: "100%",
+                        //
+                      }}
                     />
                   </a>
                 </div>
@@ -214,11 +364,19 @@ export default function Home() {
             </div>
           </div>
           <div className="flex justify-center pb-8">
-            <img
-              alt="image"
+            <Image
               className="py-2"
               src="/images/WhenYouAre.png"
-              style={{ objectFit: "contain", width: "40vw" }}
+              alt="image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority
+              style={{
+                width: "40vw",
+                height: "100%",
+                //
+              }}
             />
           </div>
         </div>
--- src/components/SwiperWrapper.tsx
+++ src/components/SwiperWrapper.tsx
@@ -1,33 +1,31 @@
-import { useCallback, useEffect, useRef } from "react"
-import Swiper from "swiper"
-import SVGIcon from "./SvgIcon"
+import { useCallback, useEffect, useRef } from "react";
+import Swiper from "swiper";
+import SVGIcon from "./SvgIcon";
 
 type Props = {
-  children: JSX.Element[]
-}
+  children: JSX.Element[];
+};
 const SwiperWrapper = (props: Props) => {
-  const swiperContainerRef = useRef<null | HTMLDivElement>(null)
-  const swiperRef = useRef<null | Swiper>(null)
+  const swiperContainerRef = useRef<null | HTMLDivElement>(null);
+  const swiperRef = useRef<null | Swiper>(null);
 
   const initSwiperCb = useCallback(() => {
-    const swiperElem = swiperContainerRef.current!
-    console.log(swiperElem)
-
+    const swiperElem = swiperContainerRef.current!;
     swiperRef.current = new Swiper(swiperElem, {
       modules: [],
       lazy: {
-        loadPrevNext: true
+        loadPrevNext: true,
       },
-      slidesPerView: 1
-    })
-  }, [])
+      slidesPerView: 1,
+    });
+  }, []);
 
   useEffect(() => {
-    initSwiperCb()
+    initSwiperCb();
     return () => {
-      swiperRef.current?.destroy(true)
-    }
-  }, [initSwiperCb])
+      swiperRef.current?.destroy(true);
+    };
+  }, [initSwiperCb]);
 
   return (
     <div className="swiper" ref={swiperContainerRef}>
@@ -39,7 +37,7 @@ const SwiperWrapper = (props: Props) => {
               {child}
               <div className="swiper-lazy-preloader"></div>
             </div>
-          )
+          );
         })}
       </div>
       <div
@@ -48,34 +46,34 @@ const SwiperWrapper = (props: Props) => {
           position: "absolute",
           zIndex: 999,
           top: "50%",
-          left: 0
+          left: 0,
           // transform: "translateY(-50%)",
         }}
         onClick={(e) => {
-          e.preventDefault()
-          swiperRef.current?.slidePrev()
+          e.preventDefault();
+          swiperRef.current?.slidePrev();
         }}
       >
         <SVGIcon name="chevron-left" color="#104d6c" />
       </div>
       <div
         onClick={(e) => {
-          e.preventDefault()
-          swiperRef.current?.slideNext()
+          e.preventDefault();
+          swiperRef.current?.slideNext();
         }}
         className="pointer"
         style={{
           position: "absolute",
           zIndex: 999,
           top: "50%",
-          right: 0
+          right: 0,
           // transform: "translateY(-50%)",
         }}
       >
         <SVGIcon name="chevron-right" color="#104d6c" size={26} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SwiperWrapper
+export default SwiperWrapper;
